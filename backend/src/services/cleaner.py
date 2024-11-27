952061d44477de8befddcbaddcc34509bac67ac9
superdevdav <david.jotian@yandex.ru>
2024-11-27 11:17:04 +0300

    print columns

--- backend/src/services/cleaner.py
+++ backend/src/services/cleaner.py
@@ -28,7 +28,9 @@ class EmailCleanerStrategy(BaseCleanStrategy):
     
       def validate_set(self, data_set):
             print(data_set.columns)
+            data_set.columns = data_set.columns.str.replace('"', '')
+            print(data_set.columns.to_list)
             
             regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-            valid_emails = data_set[data_set["contact_email"].apply(lambda x: bool(re.match(regex, str(x))))]
+            valid_emails = data_set[data_set['contact_email'].apply(lambda x: bool(re.match(regex, str(x))))]
             return valid_emails
