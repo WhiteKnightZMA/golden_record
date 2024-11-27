b1c9a6a773c14375e41349ce2e6cef23e8b153df
superdevdav <david.jotian@yandex.ru>
2024-11-27 10:22:41 +0300

    created service process

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,19 +1,17 @@
 import uvicorn
-import pandas as pd
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
+from services.processor import process_file
 
 app = FastAPI()
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
-    data = pd.read_csv(file.file)
-    data = data.drop_duplicates()
-    n = len(data)
-    output_file = "updated_file.csv"
-    data.to_csv(output_file, index=False)
+    output_file = "processed_file.csv"
+
+    _ = process_file(file.file, output_file)
     
-    return FileResponse("updated_file.csv", media_type="text/csv", filename="updated_file.csv")
+    return FileResponse(output_file, media_type="text/csv", filename=output_file)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
